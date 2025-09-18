30ef598fae93812203c1ee057be38a154faebda5
anastasiastavrou2002 <stavraki2002@gmail.com>
2025-09-18 18:00:29 +0300

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,9 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     dedupe: ['react', 'react-dom']
+  },
+  base: '/Chatbot/',   // βάλε το όνομα του repo στο GitHub
+  build: {
+    outDir: 'docs'     // να βγαίνει το build στον φάκελο docs
   }
 })
